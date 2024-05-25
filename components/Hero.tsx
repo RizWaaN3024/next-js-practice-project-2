782b764ea910ff5f44b730e24a70114be6d10788
Rizwaan <rizwaanans786@gmail.com>
2024-05-26 03:27:09 +0530

    First Commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div>
+      <img src="/logo-no-background.svg" alt="my-logo" />
+    </div>
+  )
+}
+
+export default Hero
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#01041e"
+        }
+      }
     },
   },
   plugins: [],
